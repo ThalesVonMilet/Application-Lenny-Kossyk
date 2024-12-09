4a40bea0adbaa2d744fd8bf859d90fb65e775f5e
Thales O <98899243+ThalesVonMilet@users.noreply.github.com>
2024-12-09 22:45:19 +0100

    Update cirriculum_vitae.dart

--- lib/cirriculum_vitae/cirriculum_vitae.dart
+++ lib/cirriculum_vitae/cirriculum_vitae.dart
@@ -1,14 +1,11 @@
 // Flutter imports:
 import 'package:crypto_ui_web/motivation_letter/widget/spaceing.dart';
 import 'package:flutter/material.dart';
+
 // Package imports:
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:gif/gif.dart';
 
 // Project imports:
-import '../bloc/screen_offset.dart';
-import '../motivation_letter/controller.dart';
 import '../motivation_letter/widget/long_text_block.dart';
 import '../motivation_letter/widget/subtitle_widget.dart';
 import '../motivation_letter/widget/title_widget.dart';
@@ -20,7 +17,8 @@ class CurriculumVitae extends ConsumerStatefulWidget {
   ConsumerState<CurriculumVitae> createState() => _CurriculumVitaeState();
 }
 
-class _CurriculumVitaeState extends ConsumerState<CurriculumVitae> with TickerProviderStateMixin {
+class _CurriculumVitaeState extends ConsumerState<CurriculumVitae>
+    with TickerProviderStateMixin {
   late AnimationController controller;
   late Animation<Offset> transform;
 
@@ -36,7 +34,9 @@ class _CurriculumVitaeState extends ConsumerState<CurriculumVitae> with TickerPr
       ),
     );
 
-    transform = Tween<Offset>(begin: const Offset(10, 0), end: const Offset(0, 0)).animate(CurvedAnimation(parent: controller, curve: Curves.ease));
+    transform =
+        Tween<Offset>(begin: const Offset(10, 0), end: const Offset(0, 0))
+            .animate(CurvedAnimation(parent: controller, curve: Curves.ease));
 
     controller.forward();
     super.initState();
@@ -50,45 +50,41 @@ class _CurriculumVitaeState extends ConsumerState<CurriculumVitae> with TickerPr
   }
 
   Widget _title(String text) => TitleWidget(
-    padding: const EdgeInsets.only(bottom: 25.0),
-    controller: controller,
-    text: text,
-  );
+        padding: const EdgeInsets.only(bottom: 25.0),
+        controller: controller,
+        text: text,
+      );
 
   Widget _subtitle(String text) => SubtitleWidget(
-    controller: controller,
-    text: text,
-  );
+        controller: controller,
+        text: text,
+      );
 
-  Widget _longText(String text) => LongTextBlockWidget(text: text, transform: transform);
+  Widget _longText(String text) =>
+      LongTextBlockWidget(text: text, transform: transform);
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<DisplayOffset, ScrollOffset>(
-      buildWhen: (previous, current) {
-        if (current.scrollOffsetValue > 1200 || controller.isAnimating) {
-          return true;
-        } else {
-          return false;
-        }
-      },
-      builder: (context, state) {
-        return Padding(
-          padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width*0.08),
-          child: Flex(
-            direction: Axis.vertical,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              _title('School education'),
-              _subtitle(
-                  'General qualification for university entrance'),
-              _longText('Peter Vischer School Gymnasium, Nuremberg Abitur average: 2.4'),
-              spaceUndernethSection,
-            ],
-          ),
-        );
-      },
-    );
+    return Padding(
+        padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width * 0.08),
+        child: Flex(
+          direction: Axis.vertical,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            _title('School education'),
+            _subtitle('General qualification for university entrance'),
+            _longText(
+                'Peter Vischer School Gymnasium, Nuremberg Abitur average: 2.4'),
+            spaceUndernethSection,
+          ],
+        ));
   }
 }
+
+///
+/// Capture replay Verfahren ?
+/// Arrange Act Assert !!!
+/// playwright, cyberes
+///
